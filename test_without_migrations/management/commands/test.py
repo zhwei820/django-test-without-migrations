b2b30c159cf05844318920f39068b0b40ec59147
Henrik Lindgren <henrik.lindgren@dynabyte.se>
2015-10-12 17:07:50 +0200

    Reversed flag usage to default to not use migrations.

--- test_without_migrations/management/commands/test.py
+++ test_without_migrations/management/commands/test.py
@@ -21,16 +21,16 @@ class Command(TestCommand):
         # So we only define option_list for Django 1.7
         if DJANGO_VERSION < (1, 8):
             self.option_list = super(Command, self).option_list + (
-                make_option('-n', '--nomigrations', action='store_true', dest='nomigrations', default=False,
-                    help='Tells Django to NOT use migrations and create all tables directly.'),
+                make_option('-m', '--migrations', action='store_false', dest='nomigrations', default=True,
+                    help='Tells Django to use migrations before creating tables for tests.'),
             )
 
     def add_arguments(self, parser):  # New API on Django 1.8
         super(Command, self).add_arguments(parser)
 
-        parser.add_argument('-n', '--nomigrations',
-            action='store_true', dest='nomigrations', default=False,
-            help='Tells Django to NOT use migrations and create all tables directly.'),
+        parser.add_argument('-m', '--migrations',
+            action='store_false', dest='nomigrations', default=True,
+            help='Tells Django to use migrations before creating tables for tests.'),
 
     def handle(self, *test_labels, **options):
         from django.conf import settings
